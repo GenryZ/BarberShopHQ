fea1bcfb46225cbccf220a819e53c2de1aa48865
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-02-17 20:47:56 +0300

    Add Barbers migration

--- app.rb
+++ app.rb
@@ -3,14 +3,16 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'rake'
 
 #Create database 
  
-set :database, "sqlite3:barbershop.db"
-
+set :database, {adapter:"sqlite3", database:"barbershop.db"}
 #Create entity Client
 
 class Client < ActiveRecord::Base 
+end
+class Barber < ActiveRecord::Base
 end
 
 get '/' do
