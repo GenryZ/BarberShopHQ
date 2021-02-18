e003a98f9fd044411e5c2f9e78c6e8eb6fa4d686
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-02-18 16:34:06 +0300

    comment to app.rb

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ get '/visit' do
 end
 post '/visit' do 
 	
-	c = Client.new params[:client]
+	c = Client.new params[:client]#создаем объект класса Клиент и передаем параметры в visit.erb
 	c.save
 	
 	erb "<h2> Thank you!</h2>"
