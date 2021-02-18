cbe61d2323a37a495c4515195bc3d6f5b86c144d
GenryZ <genrymorganz@gmail.com>
2021-02-18 21:16:15 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -11,6 +11,10 @@ set :database, {adapter:"sqlite3", database:"barbershop.db"}
 #Create entity Client
 
 class Client < ActiveRecord::Base 
+	validates :name, presence: true #presence: true это хэш
+	validates :phone, presence: true 
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 class Barber < ActiveRecord::Base
 end
@@ -29,6 +33,6 @@ post '/visit' do
 	
 	c = Client.new params[:client]#создаем объект класса Клиент и передаем параметры в visit.erb
 	c.save
-	
+
 	erb "<h2> Thank you!</h2>"
 end
